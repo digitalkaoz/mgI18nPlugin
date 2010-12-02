bb7d69363b28601a7cddeeb37e9950597c8fa9ce
Robert Schönthal <caziel@gmx.net>
2010-12-02 02:37:42 +0100

    cleanup

--- lib/i18nDebugPanel.class.php
+++ lib/i18nDebugPanel.class.php
@@ -2,6 +2,7 @@
 
 class i18nDebugPanel extends sfWebDebugPanel
 {
+  protected $label;
   
   public static function listenToLoadDebugWebPanelEvent(sfEvent $event)
   {
@@ -14,23 +15,24 @@ class i18nDebugPanel extends sfWebDebugPanel
     {
       $context = sfcontext::getInstance();
       $context->getConfiguration()->loadHelpers(array('I18N'));
-      $label = __('title_translation', null, 'mgI18nAdmin');
+      $this->label = __('title_translation', null, 'mgI18nAdmin');
     }
     else
     {
-      $label = 'Translations';
+      $this->label = 'Translations';
     }
-    return '<span id="translation-trigger"><img src="/images/default/grid/hmenu-asc.gif" alt="'.$label.'" height="16" width="16" />'.$label.'</span>
-      <script type="text/javascript" src="/mgI18nPlugin/js/gui.js" ></script>';
+    return '<span id="translation-trigger"><img src="/images/default/grid/hmenu-asc.gif" alt="'.$this->label.'" height="16" width="16" />'.$this->label.'</span>';
+      
   }
  
-  public function getPanelContent(){
-    return null;
+  public function getPanelContent()
+  {
+    return '<script type="text/javascript" src="/mgI18nPlugin/js/gui.js" ></script>';
   }
   
   public function getPanelTitle()
   {    
-    return ;    
+    return $this->label;    
   }
   
   public function getTitleUrl() {
